820e0d12061128ffbf035901c5643e5a9f24d1bf
Nikita Sychev <nikitasychev@inbox.ru>
2023-04-30 12:22:16 +0500

    task2 done

--- Program.cs
+++ Program.cs
@@ -12,16 +12,18 @@ namespace HomeWork4
         static void Main(string[] args)
         {
             Console.WriteLine("Task 1");
-           
-            string inputFilename = "text.txt";
-            string outputFilename = "solution.txt";
+            if (args.Length > 0)
+            {
 
-            bool appendResults = File.Exists(outputFilename);
+                string inputFilename = args[0]; // создали строку для чтения
+                string outputFilename = "solution.txt"; // создали строку для записи файла
+
+
                 try
                 {
                     using (StreamReader reader = new StreamReader(inputFilename))
                     {
-                        using (StreamWriter writer = new StreamWriter(outputFilename, appendResults))
+                        using (StreamWriter writer = new StreamWriter(outputFilename, true))
                         {
                             string line;
                             while ((line = reader.ReadLine()) != null)
@@ -35,7 +37,7 @@ namespace HomeWork4
                                         char oper = match.Groups[2].Value[0];
                                         int rightOperand = int.Parse(match.Groups[3].Value);
                                         int result;
-                                        switch (oper )
+                                        switch (oper)
                                         {
                                             case '+': result = leftOperand + rightOperand; break;
                                             case '-': result = leftOperand - rightOperand; break;
@@ -63,6 +65,75 @@ namespace HomeWork4
 
                     Console.WriteLine($"ERROR: {ex.Message}");
                 }
+            }
+            else
+            {
+                Console.WriteLine("Аргументы командной строки не переданы");
+            }
+
+            Console.WriteLine("Task 2");
+
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string inputFile = args[i];
+                    string outputFile = $"solution{i}.txt";
+                    try
+                    {
+                        using (StreamReader reader = new StreamReader(inputFile))
+                        {
+                            using (StreamWriter writer = new StreamWriter(outputFile, true))
+                            {
+                                string line;
+                                while ((line = reader.ReadLine()) != null)
+                                {
+                                    try
+                                    {
+                                        Match match = Regex.Match(line, @"(\d+)\s*([-+*/])\s*(\d+)\s*=");
+                                        if (match.Success)
+                                        {
+                                            int leftOperand = int.Parse(match.Groups[1].Value);
+                                            char oper = match.Groups[2].Value[0];
+                                            int rightOperand = int.Parse(match.Groups[3].Value);
+                                            int result;
+                                            switch (oper)
+                                            {
+                                                case '+': result = leftOperand + rightOperand; break;
+                                                case '-': result = leftOperand - rightOperand; break;
+                                                case '*': result = leftOperand * rightOperand; break;
+                                                case '/': result = leftOperand / rightOperand; break;
+                                                default: throw new InvalidOperationException($"Неизвестный оператор");
+                                            }
+                                            writer.WriteLine($"{line.Trim()} {result}");
+                                        }
+                                        else
+                                        {
+                                            writer.WriteLine($"{line.Trim()} ERROR: Invalid format");
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        writer.WriteLine($"{line.Trim()} ERROR: {ex.Message}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine($"ERROR: {ex.Message}");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is't arguments");
+            }
+
+            Console.WriteLine("Task3");
 
             Console.ReadLine();
         }
