d764fa481c62e41c6d67ec4599f71855ce7f100f
Nikita Sychev <nikitasychev@inbox.ru>
2023-04-30 12:35:11 +0500

    task3 - done

--- Program.cs
+++ Program.cs
@@ -135,6 +135,33 @@ namespace HomeWork4
 
             Console.WriteLine("Task3");
 
+            if(args.Length > 0)
+            {
+                string directory = args[0]; // аргумент командной строки - директория
+                if(Directory.Exists(directory)) // проверка
+                {
+                    Console.WriteLine("Подкаталоги:");
+                    string[] dirs = Directory.GetDirectories(directory); // получаем подкаталоги
+                    foreach (string s in dirs)
+                    {
+                        Console.WriteLine(s); //вывод
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Файлы:"); // получаем спискок файлов
+                    string[] files = Directory.GetFiles(directory);
+                    foreach (string s in files)
+                    {
+                        Console.WriteLine(s);//вывод
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(Directory.GetCurrentDirectory()); 
+            }
+
+
+            Console.WriteLine("Task4");
             Console.ReadLine();
         }
            
